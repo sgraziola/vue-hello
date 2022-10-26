208802c8bd66bbfcdc758bd5f411fd450dbaf267
sgraziola <simo.fdl@gmail.com>
2022-10-26 15:14:39 +0200

    Ex VueHello: add title

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            message: "Hello Vue!",
+        }
+    }
+}).mount("#site_header");
